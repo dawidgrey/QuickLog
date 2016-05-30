c5ab85bba6cc9a5fb0de75870e7791bd5541bf6f
Dawid Greyvenstein <dawid@thrconsulting.com.au>
2016-05-30 12:21:47 +1000

    v1

--- QuickLog/Form1.Designer.cs
+++ QuickLog/Form1.Designer.cs
@@ -28,12 +28,85 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.ui_newLog = new System.Windows.Forms.TextBox();
+            this.ui_history = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.ui_newLog);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(317, 19);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.ui_history);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 19);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(317, 248);
+            this.panel2.TabIndex = 1;
+            // 
+            // ui_newLog
+            // 
+            this.ui_newLog.BackColor = System.Drawing.Color.Gray;
+            this.ui_newLog.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.ui_newLog.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ui_newLog.ForeColor = System.Drawing.Color.Orange;
+            this.ui_newLog.Location = new System.Drawing.Point(0, 0);
+            this.ui_newLog.Name = "ui_newLog";
+            this.ui_newLog.Size = new System.Drawing.Size(317, 22);
+            this.ui_newLog.TabIndex = 0;
+            this.ui_newLog.KeyUp += new System.Windows.Forms.KeyEventHandler(this.ui_newLog_KeyUp);
+            // 
+            // ui_history
+            // 
+            this.ui_history.BackColor = System.Drawing.Color.DimGray;
+            this.ui_history.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.ui_history.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ui_history.Location = new System.Drawing.Point(0, 0);
+            this.ui_history.Multiline = true;
+            this.ui_history.Name = "ui_history";
+            this.ui_history.Size = new System.Drawing.Size(317, 248);
+            this.ui_history.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(317, 267);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.Orange;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Log";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox ui_newLog;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox ui_history;
     }
 }
 
--- QuickLog/Form1.cs
+++ QuickLog/Form1.cs
@@ -1,20 +1,44 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace QuickLog
 {
     public partial class Form1 : Form
     {
+        private readonly NLog.ILogger logger;
+
         public Form1()
         {
             InitializeComponent();
+            this.logger = NLog.LogManager.GetCurrentClassLogger();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+            e.Cancel = true;
+        }
+
+        private void ProcessMessage()
+        {
+            var message = this.ui_newLog.Text;
+            this.ui_newLog.Clear();
+            this.logger.Info(message);
+            this.ui_history.AppendText(message + "\r\n");
+            this.ui_history.ScrollToCaret();
+        }
+
+        private void ui_newLog_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                ProcessMessage();
+            }
         }
     }
 }
